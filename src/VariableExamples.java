2cbb1fdc70a72f085db648caf415520f9020622b
Emmanuel2596 <emmanuel.arguedas@ulead.ac.cr>
2020-09-26 20:05:07 -0600

    Entraga tarea 1

---
+++ src/VariableExamples.java
@@ -0,0 +1,10 @@
+public class VariableExamples {
+
+    public static void main(String[] args) {
+
+        short lowNumber = 32767;
+        long highNumber = 17179869184L; //
+        double decimalNumber = 29.5;
+
+    }
+}
